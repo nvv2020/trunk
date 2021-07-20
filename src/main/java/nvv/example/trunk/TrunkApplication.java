dcd5225ccbde07085f2c4b23a85f3aeee165a7e1
anastasiya.voronova <Voronova.Anastasiya@rt.ru>
2021-07-20 15:31:16 +0500

    My initial commit

---
+++ src/main/java/nvv/example/trunk/TrunkApplication.java
@@ -0,0 +1,13 @@
+package nvv.example.trunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nvv/example/trunk/TrunkApplicationTests.java
@@ -0,0 +1,13 @@
+package nvv.example.trunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fc7bfebd762350bc724168f9007201c67e8f9d80
ANASTASIYA VORONOVA <68094742+nvv2020@users.noreply.github.com>
2021-07-20 15:56:24 +0500

    Update TrunkApplication.java
    Add test comment

--- src/main/java/nvv/example/trunk/TrunkApplication.java
+++ src/main/java/nvv/example/trunk/TrunkApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+// Add test comment
+
 public class TrunkApplication {
 
 	public static void main(String[] args) {
